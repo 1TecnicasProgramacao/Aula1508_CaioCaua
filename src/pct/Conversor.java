071263e9f3be8b48baa8fd91d48480078937634a
cauaDelgado <cauadasilvadelgado@hotmail.com>
2020-05-29 18:52:09 -0300

    commit cauã

--- src/pct/Conversor.java
+++ src/pct/Conversor.java
@@ -1,32 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pct;
+
+import java.util.Scanner;
 
 /**
  *
- * @author caioh
+ * @author B1-08
  */
 public class Conversor {
     public static void main(String[] args) {
-        //Declaração de variaveis
+        //Declaração de variáveis
+        int n1, n2, total;
         
+        //Objeto de entrada
+        Scanner entrada = new Scanner(System.in);
         
-        //Criação e instância do objeto de entrada
-        
-        
+        System.out.println("\n\t\t\t -- Calculadora --\n");
         //Entrada
-        
+        System.out.println("Soma Simples");
+        System.out.print("Informe n1: ");
+        n1 = entrada.nextInt();
+        System.out.print("Informe n2: ");
+        n2 = entrada.nextInt();
         
         //Processamento
+        total = n1 + n2;
         
         
         //Saída
+        System.out.println("\n\t " + n1 + " + " + n2 + " = " + total);
+        
+        
         
         
         
     }
+    
     
 }
